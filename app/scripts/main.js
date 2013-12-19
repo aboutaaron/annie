4df8e4bf8ed43dc77f772030dee4b98a47651e1f
aboutaaron <aaron.colby.williams@gmail.com>
2013-12-19 12:38:58 -0800

    some base css and js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,25 @@
-console.log('\'Allo \'Allo!');
+var App = App || {};
+
+App = {
+  config: {
+    url: 'https://docs.google.com/spreadsheet/pub?key=0AoFyAyn0Wp26dG1nWDlxY3Q5a0U1aGJvdm1YQUNMZ2c&output=html'
+  },
+  run: function () {
+    jQuery(document).ready(function($) {
+      App.init();
+    });
+  },
+  init: function () {
+    Tabletop.init( {
+      key: App.config.url,
+      callback: App.build,
+      simpleSheet: true
+    });
+  },
+  build: function (data, tabletop) {
+    console.log(data);
+  }
+}
+
+// Run
+App.run();
